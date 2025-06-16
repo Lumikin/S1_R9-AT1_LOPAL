0b6c914f7ae856c7cf26968d6af6dfa743bae7dd
Lumikin <lumikomio04@gmail.com>
2025-06-16 15:59:23 -0300

    Codigo atualizado

--- calculadoraNode.js
+++ calculadoraNode.js
@@ -3,21 +3,25 @@ const promptSync = require("prompt-sync")();
 const path = require("path");
 const PromptSync = require("prompt-sync");
 let i = 0; i <= 0; i++
-let entrega, urgente, normal, encerrar, valor, Distancia, Pessoa
+let prompt, entrega, urgente, normal, encerrar, valor, Distancia, Pessoa
+
 
 do {
 
-    Pessoa = promptSync(("Digite o nome da pessoa ${i}: ")) //Atribuir o nome da pessoa
+    Pessoa = promptSync((`Digite o nome da pessoa ${i}: `)) //Atribuir o nome da pessoa
     valor = promptSync((("digite o valor por kilometro: "))) // o valor do quilometro
     Distancia = promptSync((("digite a distancia em Kilometros: ")))// e a distancia em kilometro 
     entrega = promptSync((("Tipo de entrega (1 para normal e 0 para urgente): "))) // Se for 1 vai ser Normal e 0 para urgente)
-    let processamento = valor * Distancia
+    
     if (entrega == 1) {
-        console.log(processamento)
+        let processamento = valor * Distancia
+        console.log(`${processamento}`)
            
 
         // fs.writeFileSync(Arquivos, Conteudo, "utf-8" 
 
+    } else{
+        console.log("awwwaa")
     }
 } while ((isNaN(valor)) && (isNaN(Distancia)));
 
