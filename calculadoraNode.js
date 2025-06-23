4a69732f8d746a787ef67978fffff5dd0df31d7a
Lumikin <lumikomio04@gmail.com>
2025-06-23 15:17:28 -0300

    Teste-final

--- calculadoraNode.js
+++ calculadoraNode.js
@@ -1,3 +1,4 @@
+const { Console } = require("console");
 const fs = require("fs");
 const { console } = require("inspector");
 const promptSync = require("prompt-sync")();
@@ -11,47 +12,44 @@ let Distancia = [];
 let pessoa = [];
 let valorTotal = 0;
 let tipoUrgente
-
+let entrega = []
 
 
 do {
 
-    pessoa = promptSync((`Digite o nome da pessoa ${i+1}: `)) //Atribuir o nome da pessoa
+    pessoa = promptSync((`Digite o nome da pessoa ${i}: `)) //Atribuir o nome da pessoa
     valor = promptSync((("digite o valor por kilometro: "))) // o valor do quilometro
     distancia = promptSync((("digite a distancia em Kilometros: ")))// e a distancia em kilometro 
     tipoUrgente = promptSync((("Tipo de entrega (1 para normal e 0 para urgente): "))) // Se for 1 vai ser Normal e 0 para urgente)
 
-    
-        if (tipoUrgente == 0) { // Estrutura condicional
+    Distancia[i] += distancia
 
-            valorEntregas[i] = distancia * (valor * 1.2); // Calculo caso a entrega seja urgente.
-            console.log(`dista: ${distancia}`);
-            console.log(`val: ${valor}`);
-            console.log(`Entrega if: ${valorEntregas[i]}`);
+    valorEntregas[i] = distancia * valor;
+    if (tipoUrgente == 0) { // Estrutura condicional
 
-
-        } else {
-
-            valorEntregas[i] = distancia * valor; // Calculo caso a entrega seja normal.
+        valorEntregas[i] = valorEntregas[i] * 1.2; // Calculo caso a entrega seja urgente.
+        console.log(`dista: ${distancia}`);
+        console.log(`val: ${valor}`);
+        console.log(`Entrega if: ${valorEntregas[i]}`);
 
 
-        }
-        valorTotal += valorEntregas[i]; // Calculo do valor Total (total de entregas).
-        console.log(`Entregas: ${valorEntregas}`);
-        console.log(`i: ${i}`);
-        console.log(`Valor total: ${valorTotal}`);
-        encerrar = promptSync("deseja encerrar? 0 para sim e 1 para não: ");
-        i++;
-
-   
+    }
+    valorTotal += valorEntregas[i]; // Calculo do valor Total (total de entregas).
+    console.log(`Entregas: ${valorEntregas}`);
+    console.log(`i: ${i}`);
+    console.log(`Valor total: ${valorTotal}`);
+    encerrar = promptSync("deseja encerrar? 0 para sim e 1 para não: ");
+    i++;
 
 
-
-} while ( encerrar == 1);
-
-    console.log(valorEntregas)
     media = valorTotal / i; // Calculo média, após o calculo do valor total.
-    console.log(media);
     valorTotal = valorTotal; // Aredondamento para 2 casas decimais
     media = media; // Arredondamento para 2 casas decimais
 
+
+} while (encerrar == 1);
+
+
+let dados = `\n O(a) ${pessoa}\n tem a distancia de ${Distancia}Km\n ${media}\n ${valorTotal}`;
+
+fs.appendFileSync("dados.txt", dados, "utf8")
